754cd4d56cf68ef73a7f030eea2c563b59e609bc
Susana Martinez Linares <sml175@hotmail.com>
2015-07-26 14:08:25 +0200

    Editar preguntas

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var partials = require('express-partials');
 var routes = require('./routes/index');
+var methodOverride = require('method-override');
 //var users = require('./routes/users');
 
 var app = express();
@@ -23,8 +23,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
 //app.use('/users', users);
 
