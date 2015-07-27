4be8e4eeca85d912f83835dcc43c12e780bed1d0
Susana Martinez Linares <sml175@hotmail.com>
2015-07-27 19:51:13 +0200

    Autenticación y sesión

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var partials = require('express-partials');
 var routes = require('./routes/index');
 var methodOverride = require('method-override');
-//var users = require('./routes/users');
+var session = require('express-session');
 
 var app = express();
 
@@ -22,9 +22,28 @@ app.use(partials());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(cookieParser());
+app.use(cookieParser('Quiz 2015'));
+//app.use(session());
+app.use(session({
+  secret: 'Quiz 2015',
+  resave: false,
+  saveUninitialized: true
+}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Helpers dinamicos:
+app.use(function(req, res, next) {
+  // guardar path en session.redir para despues de login
+  if (!req.path.match(/\/login|\/logout|\/user/)) {
+    req.session.redir = req.path;
+  }
+
+  // Hacer visible req.session en las vistas
+  res.locals.session = req.session;
+  next();
+});
+
 app.use('/', routes);
 //app.use('/users', users);
 
